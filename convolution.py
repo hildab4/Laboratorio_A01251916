93e4acab48d518bb33a1518160468e0501516a30
Hilda Beltrán <hildabeltran@Hildas-MacBook-Pro.local>
2021-05-05 19:37:38 -0500

    Agregar imagen a convolución

--- convolution.py
+++ convolution.py
@@ -1,13 +1,14 @@
+#Importar librerías
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-#mat = np.matrix('1 2 3 4 5 6; 7 8 9 10 11 12; 0 0 1 16 17 18; 0 1 0 7 23 24; 1 7 6 5 4 3')
-#kernel = np.matrix('1 1 1; 0 0 0; 2 10 3')
+#Nombre archivo = foto.jpg
+#Filtro = [[-1 0 1]; [-2 0 2]; [-1 0 1]]
 
-def convolucion (mat, kernel):
+def convolucion (img, kernel):
     #Convierte ambas entradas a matrices con ayuda de numpy
-    mat = np.matrix(mat)
+    mat = np.matrix(img)
     kernel = np.matrix(kernel)
     #Obtiene las dimensiones de la matriz resultante
     row = (np.shape(mat)[0] - (np.shape(kernel)[0]/2) * 2) + 1
@@ -28,11 +29,14 @@ def convolucion (mat, kernel):
             #Se le asigna el valor de cero a la variable, para repetir el proceso
             num1 = 0
     #Se imprime la matriz resultante
-    print(final)
+    plt.imshow(final, cmap='gray')
+    plt.title("Imagen usando Kernel")
+    plt.show()
 
-#Pide al usuario la matriz original y el filtro a aplicar
-mat = input('Matriz 1')
-kernel = input('Filtro')
+#Pide al usuario la matriz original (imagen) y el filtro a aplicar
+imagen = input('Nombre archivo ')
+img = cv2.imread(imagen, cv2.IMREAD_GRAYSCALE)
+kernel = input('Filtro ')
 #Llama a la función, mandando ambas matrices
-convolucion(mat, kernel)
+convolucion(img, kernel)
         
